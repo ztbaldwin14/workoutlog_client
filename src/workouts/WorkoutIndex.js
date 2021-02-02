e30e30d6cb70ae688306a24072cbc09e95d2185a
apple <apple@apples-MacBook-Air.local>
2021-02-01 23:05:58 -0500

    completed client modules

--- src/workouts/WorkoutIndex.js
+++ src/workouts/WorkoutIndex.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import WorkoutCreate from './WorkoutCreate';
+import WorkoutTable from './WorkoutTable';
+import WorkoutEdit from './WorkoutEdit';
 
 const WorkoutIndex = (props) => {
     const [workouts, setWorkouts] = useState([]);
+    const [updateActive, setUpdateActive] = useState(false);
+    const [workoutToUpdate, setWorkoutToUpdate] = useState({});
+
     const fetchWorkouts = () => {
         fetch('http://localhost:3000/log', {
             method: 'GET',
@@ -15,22 +21,35 @@ const WorkoutIndex = (props) => {
                 setWorkouts(logData)
             })
     }
+    const editUpdateWorkout = (workout) => {
+        setWorkoutToUpdate(workout);
+        console.log(workout);
+    }
+
+    const updateOn = () => {
+        setUpdateActive(true);
+    }
+
+    const updateOff = () => {
+        setUpdateActive(false);
+    }
 
     useEffect(() => {
         fetchWorkouts();
     }, [])
 
     return (
-        <Containers>
+        <Container>
             <Row>
                 <Col md="3">
-                    {/*The create component will go here */}
+                    <WorkoutCreate fetchWorkouts={fetchWorkouts} token={props.token} />
                 </Col>
                 <Col md="9">
-                    <h2>Log a workout to see a table. This will be added in later pages.</h2>
+                    <WorkoutTable workouts={workouts} editUpdateWorkout={editUpdateWorkout} updateOn={updateOn} fetchWorkouts={fetchWorkouts} token={props.token} />
                 </Col>
+                {updateActive ? <WorkoutEdit workoutToUpdate={workoutToUpdate} updateOff={updateOff} token={props.token} fetchWorkouts={fetchWorkouts} /> : <></>}
             </Row>
-        </Containers>
+        </Container>
     )
 }
 
